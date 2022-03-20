0b9972e6b063f26dd1e865a85df9cb7efc6391be
Kyle Curry <kylecurry43@gmail.com>
2022-03-20 15:56:19 -0500

    added sudoku board generator

--- src/context/sudokuContext.js
+++ src/context/sudokuContext.js
@@ -1,3 +1,4 @@
+import sudokuGenerator from "../util/sudokuGenerator";
 import createDataContext from "./createDataContext";
 
 const reducer = (state, action) => {
@@ -59,16 +60,7 @@ const setTileSelected = (dispatch) => (row, column, value=true) => {
 
 
 /* initialBoardState */
-let initialBoardState = 
-[[8,3,5,4,1,6,9,2,7],
- [2,9,6,8,5,7,4,3,1],
- [4,1,7,2,9,3,6,5,8],
- [5,6,9,1,3,4,7,8,2],
- [1,2,3,6,7,8,5,4,9],
- [7,4,8,5,2,9,1,6,3],
- [6,5,2,7,8,1,3,9,4],
- [9,8,1,3,4,5,2,7,6],
- [3,7,4,9,6,2,8,1,5]];
+let initialBoardState = sudokuGenerator();
 
 
  initialBoardState = initialBoardState.map((row, rowIndex) => {
---
+++ src/util/sudokuGenerator.js
@@ -0,0 +1,101 @@
+function shuffleRows(board, n=3) {
+
+    for (let boxRow = 0; boxRow < 3; boxRow++) {
+
+        for (let i = 0; i < n; i++) {
+
+            const row1 = Math.floor(Math.random() * 3);
+
+            let row2 = Math.floor(Math.random() * 3);
+            while (row1 === row2) {
+                row2 = Math.floor(Math.random() * 3);
+            }
+
+            [board[boxRow*3+row1], board[boxRow*3+row2]] = [board[boxRow*3+row2], board[boxRow*3+row1]];
+        }
+    }
+
+    return board;
+}
+
+function shuffleColumns(board, n=3) {
+
+    for (let boxCol = 0; boxCol < 3; boxCol++) {
+
+
+        for (let i = 0; i < n; i++) {
+
+            const col1 = Math.floor(Math.random() * 3);
+
+            let col2 = Math.floor(Math.random() * 3);
+            while (col1 === col2) {
+                col2 = Math.floor(Math.random() * 3);
+            }
+
+
+            for (let row = 0; row < 9; row++) {
+                [board[row][boxCol*3+col1], board[row][boxCol*3+col2]] = [board[row][boxCol*3+col2], board[row][boxCol*3+col1]];
+            }
+        }
+    }
+
+    return board;
+}
+
+function rotateBoard(board, n=null) {
+
+    if (n === null) {
+        n = Math.floor(Math.random() * 4);
+    }
+    console.log(n);
+
+    for (let rotateCount = 0; rotateCount < n; rotateCount++) {
+        const n = board.length;
+        const boardCopy = board.map(row => {
+            return row.slice();
+        });
+    
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n; j++) {
+
+              let ele = boardCopy[i][j];
+              let idx = n - (i + 1)
+              
+              board[j][idx] = ele
+            }
+        }
+    }
+
+    return board;
+}
+
+function sudokuShuffle(board) {
+    shuffleRows(board);
+    shuffleColumns(board);
+    rotateBoard(board);
+    return board;
+}
+
+
+
+function sudokuGenerator() {
+
+    const board = 
+    [
+        [8,3,5,4,1,6,9,2,7],
+        [2,9,6,8,5,7,4,3,1],
+        [4,1,7,2,9,3,6,5,8],
+        [5,6,9,1,3,4,7,8,2],
+        [1,2,3,6,7,8,5,4,9],
+        [7,4,8,5,2,9,1,6,3],
+        [6,5,2,7,8,1,3,9,4],
+        [9,8,1,3,4,5,2,7,6],
+        [3,7,4,9,6,2,8,1,5]
+    ];
+
+    sudokuShuffle(board);
+
+    return board;
+}
+
+export default sudokuGenerator;
